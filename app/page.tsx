885eddd4e01069ce848a9aa2a7d1e4cc44e53d72
Aazib Chaudhry <aazibch47@gmail.com>
2024-07-22 17:39:44 +0500

    Create recent-articles component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import { HiInformationCircle } from 'react-icons/hi';
 import { Alert } from 'flowbite-react';
 import Header from '@/components/layout/header/header';
-import HeroCarousel from '@/components/hero-carousel/hero-carousel';
+import HeroCarousel from '@/components/home/hero-carousel/hero-carousel';
+import RecentArticles from '@/components/home/recent-articles/recent-articles';
 
 export default function Home() {
-  return <HeroCarousel />;
+  return (
+    <>
+      <HeroCarousel />
+      <RecentArticles />
+    </>
+  );
 }
