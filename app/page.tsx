bb07ba449f24f82f7bb4d71d2a272657e0c71129
Aazib Chaudhry <aazibch47@gmail.com>
2024-08-01 23:07:36 +0500

    Create about-excerpt section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/layout/header/header';
 import HeroCarousel from '@/components/home/hero-carousel/hero-carousel';
 import RecentArticles from '@/components/home/recent-articles/recent-articles';
 import RecentGhazalEntries from '@/components/home/recent-ghazal-entries/recent-ghazal-entries';
+import AboutExcerpt from '@/components/home/about-excerpt/about-excerpt';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HeroCarousel />
       <RecentGhazalEntries />
       <RecentArticles />
+      <AboutExcerpt />
     </>
   );
 }
---
+++ components/home/about-excerpt/about-excerpt.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { Button } from 'flowbite-react';
+
+export default function AboutExcerpt() {
+  return (
+    <div className="p-8 bg-[url('/images/shahi-hammam-main-dome-overlay.png')] bg-no-repeat bg-center bg-cover">
+      <div className="container mx-auto my-10 text-white">
+        <h1 className="uppercase text-center text-xl mb-12">
+          Learn More About Us
+        </h1>
+        <div className="max-w-[50rem] mx-auto text-center">
+          <p className="text-xl mb-10">
+            The Ghazal Project was started with the goal of promoting the rich
+            artistic culture of Pakistan and the subcontinent, and to encourage
+            cultural emalgamation through a variant of the ghazal form, fit for
+            adaption into English poetry, and to this end, we have partnered
+            with Virsapur, an organization dedicated to reviving Pakistan's
+            diverse cultural heritage.
+          </p>
+
+          <div className="flex justify-center">
+            <Button color="blue" gradientDuoTone="purpleToBlue" size="lg" pill>
+              Learn More
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
