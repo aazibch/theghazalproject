22bd19686cfd14e33f06737a56e0781237c330d9
Aazib Chaudhry <aazibch47@gmail.com>
2024-08-30 10:13:41 +0500

    Create the nav-link component

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -1,5 +1,44 @@
-import NavigationBar from './navigation-bar/navigation-bar';
+'use client';
+
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import NavLink from './nav-link';
 
 export default function Header() {
-  return <NavigationBar />;
+  return (
+    <Navbar
+      theme={{ link: { active: { on: 'text-primary_blue' } } }}
+      rounded
+      border
+    >
+      <Navbar.Brand href="/">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-primary_blue font-crimson_text">
+          The Ghazal Project
+        </span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={<Avatar alt="User settings" rounded />}
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Aazib Chaudhry</span>
+            <span className="block truncate text-sm font-medium">
+              aazibch@theghazalproject.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/articles">Articles</NavLink>
+        <NavLink href="#">Contribute</NavLink>
+        <NavLink href="#">About Us</NavLink>
+        <NavLink href="#">Contact</NavLink>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
---
+++ components/layout/header/nav-link.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Link from 'next/link';
+import { Navbar } from 'flowbite-react';
+import { usePathname } from 'next/navigation';
+
+interface NavLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+export default function NavLink({ href, children }: NavLinkProps) {
+  const path = usePathname();
+
+  console.log('href', href);
+  console.log('path', path);
+
+  return (
+    <Navbar.Link
+      as={Link}
+      href={href}
+      active={
+        (path.startsWith(href) && href !== '/') ||
+        (path === '/' && href === '/')
+      }
+    >
+      {children}
+    </Navbar.Link>
+  );
+}
