bb74ef6735bc8460fe48dfcaff349723a9fcf573
Aazib Chaudhry <aazibch47@gmail.com>
2024-09-11 21:25:51 +0500

    Adjust responsive styling for hero-section

--- components/home/hero-section/hero-section.tsx
+++ components/home/hero-section/hero-section.tsx
@@ -4,9 +4,9 @@ import heroBanner from '@/assets/images/hero-banner.png';
 
 export default function HeroSection() {
   return (
-    <div className="flex h-[30rem]">
+    <div className="flex h-[30rem] bg-[#edebe6]">
       <Image
-        className="block w-[full] object-cover"
+        className="block w-[full] object-cover mx-auto"
         src={heroBanner}
         alt='Hero banner with the text, "Where you can learn about the history of the ghazal,
 and contribute your own verses."'
