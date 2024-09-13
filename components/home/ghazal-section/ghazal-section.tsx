50bf6abf3afbe87e05bda73d7e51c1977246c679
Aazib Chaudhry <aazibch47@gmail.com>
2024-09-14 01:23:50 +0500

    Adjust styling

--- components/home/ghazal-section/ghazal-section.tsx
+++ components/home/ghazal-section/ghazal-section.tsx
@@ -5,88 +5,75 @@ export default function GhazalSection() {
     <div className="container mx-auto text-center py-16">
       <h2 className="uppercase mb-8 text-xl ">The Collective Ghazal</h2>
       <h3 className="text-lg mb-4 text-gray-500">Recent Entries</h3>
+
       <div>
-        <div className="flex justify-center items-center mb-4">
-          <div className="mr-6">
+        <div className="flex items-center justify-center mb-4 mx-auto">
+          <div className="basis-[8rem] mr-2">
             <Avatar
               rounded
               img="https://flowbite-react.com/images/people/profile-picture-1.jpg"
             />
             <span className="text-sm text-gray-600">Omar</span>
           </div>
-          <div className="font-merriweather text-gray-900 font-light">
+          <div className="font-merriweather font-light basis-[25rem]">
             <p>To understand my words one must know silence,</p>
             <p>For these words emerge from the womb of silence.</p>
           </div>
         </div>
 
-        <div className="flex justify-center items-center mb-4">
-          <div className="mr-6">
+        <div className="flex items-center justify-center mb-4 mx-auto">
+          <div className="basis-[8rem] mr-2">
             <Avatar
               rounded
-              img="https://flowbite-react.com/images/people/profile-picture-1.jpg"
+              img="https://flowbite-react.com/images/people/profile-picture-2.jpg"
             />
-            <span className="text-sm text-gray-600">Omar</span>
+            <span className="text-sm text-gray-600">Evan Mitchell</span>
           </div>
-          <div className="font-merriweather text-gray-900 font-light">
+          <div className="font-merriweather font-light basis-[25rem]">
             <p>In self-exile, I speak to no one but you, beloved:</p>
             <p>You who are the melodious voice of silence.</p>
           </div>
         </div>
 
-        <div className="flex justify-center items-center mb-4">
-          <div className="mr-6">
+        <div className="flex items-center justify-center mb-4 mx-auto">
+          <div className="basis-[8rem] mr-2">
             <Avatar
               rounded
-              img="https://flowbite-react.com/images/people/profile-picture-1.jpg"
+              img="https://flowbite-react.com/images/people/profile-picture-3.jpg"
             />
-            <span className="text-sm text-gray-600">Omar</span>
+            <span className="text-sm text-gray-600">Liam Anderson</span>
           </div>
-          <div className="font-merriweather text-gray-900 font-light">
+          <div className="font-merriweather font-light basis-[25rem]">
             <p>If truth needs to be confirmed by perception,</p>
             <p>How am I to affirm your existence, O Silence?</p>
           </div>
         </div>
 
-        <div className="flex justify-center items-center mb-4">
-          <div className="mr-6">
+        <div className="flex items-center justify-center mb-4 mx-auto">
+          <div className="basis-[8rem] mr-2">
             <Avatar
               rounded
-              img="https://flowbite-react.com/images/people/profile-picture-1.jpg"
+              img="https://flowbite-react.com/images/people/profile-picture-4.jpg"
             />
-            <span className="text-sm text-gray-600">Omar</span>
+            <span className="text-sm text-gray-600">Amina Hassan</span>
           </div>
-          <div className="font-merriweather text-gray-900 font-light">
+          <div className="font-merriweather font-light basis-[25rem]">
             <p>Meditation is just another ritual, I am aware.</p>
             <p>True piety is in the act of chanting silence.</p>
           </div>
         </div>
 
-        <div className="flex justify-center items-center mb-4">
-          <div className="mr-6">
+        <div className="flex items-center justify-center mb-4 mx-auto">
+          <div className="basis-[8rem] mr-2">
             <Avatar
               rounded
-              img="https://flowbite-react.com/images/people/profile-picture-1.jpg"
+              img="https://flowbite-react.com/images/people/profile-picture-5.jpg"
             />
-            <span className="text-sm text-gray-600">Omar</span>
+            <span className="text-sm text-gray-600">Finn</span>
           </div>
-          <div className="font-merriweather text-gray-900 font-light">
-            <p>Where perception fails, love lights the way.</p>
-            <p>The lantern of love is naught but flaming silence.</p>
-          </div>
-        </div>
-
-        <div className="flex justify-center items-center mb-4">
-          <div className="mr-6">
-            <Avatar
-              rounded
-              img="https://flowbite-react.com/images/people/profile-picture-1.jpg"
-            />
-            <span className="text-sm text-gray-600">Omar</span>
-          </div>
-          <div className="font-merriweather text-gray-900 font-light">
-            <p>If death be near, I will embrace it with open arms.</p>
-            <p>Having lived it, why should I fear to die in silence?</p>
+          <div className="font-merriweather font-light basis-[25rem]">
+            <p>Meditation is just another ritual, I am aware.</p>
+            <p>True piety is in the act of chanting silence.</p>
           </div>
         </div>
       </div>
