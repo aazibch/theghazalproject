28f3860fc8ca145f479bba8c2862ed967075d090
Aazib Chaudhry <aazibch47@gmail.com>
2024-09-16 03:20:21 +0500

    Create ghazal-section component

---
+++ components/home/ghazal-section/ghazal-couplet.tsx
@@ -0,0 +1,29 @@
+import { Avatar } from 'flowbite-react';
+
+interface UserObj {
+  avatar: string;
+  username: string;
+}
+
+type Couplet = [string, string];
+
+export default function GhazalCouplet({
+  user,
+  couplet
+}: {
+  user: UserObj;
+  couplet: Couplet;
+}) {
+  return (
+    <div className="flex items-center justify-center mb-4 mx-auto">
+      <div className="basis-[8rem] mr-2">
+        <Avatar rounded img={user.avatar} />
+        <span className="text-sm text-gray-600">{user.username}</span>
+      </div>
+      <div className="font-merriweather font-light basis-[25rem]">
+        <span className="block">{couplet[0]}</span>
+        <span className="block">{couplet[1]}</span>
+      </div>
+    </div>
+  );
+}
--- components/home/ghazal-section/ghazal-section.tsx
+++ components/home/ghazal-section/ghazal-section.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Button } from 'flowbite-react';
+import GhazalCouplet from './ghazal-couplet';
 
 export default function GhazalSection() {
   return (
@@ -6,76 +7,71 @@ export default function GhazalSection() {
       <h2 className="uppercase mb-8 text-xl ">The Collective Ghazal</h2>
       <h3 className="text-lg mb-4 text-gray-500">Recent Entries</h3>
 
-      <div>
-        <div className="flex items-center justify-center mb-4 mx-auto">
-          <div className="basis-[8rem] mr-2">
-            <Avatar
-              rounded
-              img="https://flowbite-react.com/images/people/profile-picture-1.jpg"
-            />
-            <span className="text-sm text-gray-600">Omar</span>
-          </div>
-          <div className="font-merriweather font-light basis-[25rem]">
-            <p>To understand my words one must know silence,</p>
-            <p>For these words emerge from the womb of silence.</p>
-          </div>
-        </div>
+      <div className="mb-8">
+        <GhazalCouplet
+          user={{
+            username: 'Omar',
+            avatar:
+              'https://flowbite-react.com/images/people/profile-picture-1.jpg'
+          }}
+          couplet={[
+            'To understand my words one must know silence,',
+            'For these words emerge from the womb of silence.'
+          ]}
+        />
+        <GhazalCouplet
+          user={{
+            username: 'Evan Mitchell',
+            avatar:
+              'https://flowbite-react.com/images/people/profile-picture-2.jpg'
+          }}
+          couplet={[
+            'In self-exile, I speak to no one but you, beloved:',
+            'You who are the melodious voice of silence.'
+          ]}
+        />
+        <GhazalCouplet
+          user={{
+            username: 'Liam Anderson',
+            avatar:
+              'https://flowbite-react.com/images/people/profile-picture-3.jpg'
+          }}
+          couplet={[
+            'If truth needs to be confirmed by perception,',
+            'How am I to affirm your existence, O Silence?'
+          ]}
+        />
+        <GhazalCouplet
+          user={{
+            username: 'Amina Hassan',
+            avatar:
+              'https://flowbite-react.com/images/people/profile-picture-4.jpg'
+          }}
+          couplet={[
+            'Meditation is just another ritual, I am aware.',
+            'True piety is in the act of chanting silence.'
+          ]}
+        />
+        <GhazalCouplet
+          user={{
+            username: 'Finn',
+            avatar:
+              'https://flowbite-react.com/images/people/profile-picture-5.jpg'
+          }}
+          couplet={[
+            'Meditation is just another ritual, I am aware.',
+            'True piety is in the act of chanting silence.'
+          ]}
+        />
+      </div>
 
-        <div className="flex items-center justify-center mb-4 mx-auto">
-          <div className="basis-[8rem] mr-2">
-            <Avatar
-              rounded
-              img="https://flowbite-react.com/images/people/profile-picture-2.jpg"
-            />
-            <span className="text-sm text-gray-600">Evan Mitchell</span>
-          </div>
-          <div className="font-merriweather font-light basis-[25rem]">
-            <p>In self-exile, I speak to no one but you, beloved:</p>
-            <p>You who are the melodious voice of silence.</p>
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center mb-4 mx-auto">
-          <div className="basis-[8rem] mr-2">
-            <Avatar
-              rounded
-              img="https://flowbite-react.com/images/people/profile-picture-3.jpg"
-            />
-            <span className="text-sm text-gray-600">Liam Anderson</span>
-          </div>
-          <div className="font-merriweather font-light basis-[25rem]">
-            <p>If truth needs to be confirmed by perception,</p>
-            <p>How am I to affirm your existence, O Silence?</p>
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center mb-4 mx-auto">
-          <div className="basis-[8rem] mr-2">
-            <Avatar
-              rounded
-              img="https://flowbite-react.com/images/people/profile-picture-4.jpg"
-            />
-            <span className="text-sm text-gray-600">Amina Hassan</span>
-          </div>
-          <div className="font-merriweather font-light basis-[25rem]">
-            <p>Meditation is just another ritual, I am aware.</p>
-            <p>True piety is in the act of chanting silence.</p>
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center mb-4 mx-auto">
-          <div className="basis-[8rem] mr-2">
-            <Avatar
-              rounded
-              img="https://flowbite-react.com/images/people/profile-picture-5.jpg"
-            />
-            <span className="text-sm text-gray-600">Finn</span>
-          </div>
-          <div className="font-merriweather font-light basis-[25rem]">
-            <p>Meditation is just another ritual, I am aware.</p>
-            <p>True piety is in the act of chanting silence.</p>
-          </div>
-        </div>
+      <p className="mt-8 mb-4">
+        Learn more about the collective ghazal project or contribute your own
+        verses.
+      </p>
+      <div className="flex gap-2 justify-center">
+        <Button>Learn More</Button>
+        <Button color="blue">Contribute</Button>
       </div>
     </div>
   );
