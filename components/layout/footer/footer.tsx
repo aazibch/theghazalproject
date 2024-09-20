3d0bef066ac6e24fcc2b91f56eefa67fc2ca9843
Aazib Chaudhry <aazibch47@gmail.com>
2024-09-20 12:15:49 +0500

    Style footer component

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -9,39 +9,36 @@ const customLinkTheme = {
 
 export default function FooterComponent() {
   return (
-    <Footer className="rounded-none" bgDark>
+    <Footer className="rounded-none" container bgDark>
       <div className="w-full text-center">
-        <div className="px-6 py-8">
-          <div className="container mx-auto">
-            <div className="w-full items-center justify-between sm:flex sm:items-center sm:justify-between">
-              <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-primary_blue font-crimson_text">
-                The Ghazal Project
-              </span>
-              <Footer.LinkGroup>
-                <Footer.Link as={Link} theme={customLinkTheme} href="#">
-                  About
-                </Footer.Link>
-                <Footer.Link as={Link} theme={customLinkTheme} href="#">
-                  Privacy Policy
-                </Footer.Link>
-                <Footer.Link as={Link} theme={customLinkTheme} href="#">
-                  Contact
-                </Footer.Link>
-              </Footer.LinkGroup>
-            </div>
+        <div className="container mx-auto">
+          <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+            <span className="block mb-2 sm:mb-0 self-center whitespace-nowrap text-xl font-semibold dark:text-white text-primary_blue font-crimson_text">
+              The Ghazal Project
+            </span>
+            <Footer.LinkGroup className="flex justify-center">
+              <Footer.Link as={Link} theme={customLinkTheme} href="#">
+                About
+              </Footer.Link>
+              <Footer.Link as={Link} theme={customLinkTheme} href="#">
+                Privacy Policy
+              </Footer.Link>
+              <Footer.Link as={Link} theme={customLinkTheme} href="#">
+                Contact
+              </Footer.Link>
+            </Footer.LinkGroup>
           </div>
         </div>
-        <div className="bg-gray-700 text-gray-500 px-4 py-6">
-          <div className="container mx-auto">
-            <Footer.Copyright
-              theme={{
-                href: 'ml-1 text-gray-500 hover:underline'
-              }}
-              href="/"
-              by="The Ghazal Project"
-              year={2024}
-            />
-          </div>
+        <Footer.Divider className="border-gray-700" />
+        <div className="container mx-auto">
+          <Footer.Copyright
+            theme={{
+              href: 'ml-1 hover:underline text-gray-500'
+            }}
+            href="#"
+            by="The Ghazal Project"
+            year={2024}
+          />
         </div>
       </div>
     </Footer>
