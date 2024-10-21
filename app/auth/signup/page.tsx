ea5e372c54d54137e65bf016e5310b1c14925960
Aazib Chaudhry <aazibch47@gmail.com>
2024-10-21 15:03:15 +0500

    Create signup page

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -4,11 +4,33 @@ import { Button, Label, TextInput } from 'flowbite-react';
 import PageHeader from '@/components/layout/pages/page-header';
 import styles from '../page.module.css';
 
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <div className="container mx-auto my-12">
-      <PageHeader heading="Login" />
+      <PageHeader heading="Signup" />
       <form className="flex max-w-md flex-col gap-4 mx-auto">
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="fullName" value="Full Name" />
+          </div>
+          <TextInput
+            id="fullName"
+            type="text"
+            placeholder="John Doe"
+            required
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="username" value="Username" />
+          </div>
+          <TextInput
+            id="username"
+            type="text"
+            placeholder="JohnTheBard"
+            required
+          />
+        </div>
         <div>
           <div className="mb-2 block">
             <Label htmlFor="email" value="Email" />
@@ -26,6 +48,12 @@ export default function LoginPage() {
           </div>
           <TextInput id="password" type="password" required />
         </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="passwordConfirmation" value="Confirm Password" />
+          </div>
+          <TextInput id="passwordConfirmation" type="password" required />
+        </div>
 
         <Button color="blue" type="submit">
           Submit
@@ -34,11 +62,11 @@ export default function LoginPage() {
           <p
             className={`${styles.authAlternativeMessage} text-gray-500 text-sm mb-5`}
           >
-            Don't have an account?
+            Already have an account?
           </p>
-          <Link className="hover:no-underline block w-full" href="/auth/signup">
+          <Link className="hover:no-underline block w-full" href="/auth/login">
             <Button outline color="blue" className="block w-full">
-              Signup Instead
+              Login Instead
             </Button>
           </Link>
         </div>
