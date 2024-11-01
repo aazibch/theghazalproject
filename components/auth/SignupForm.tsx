6fa0b40994528b73712718a0a00b2a86fbbf1bed
Aazib Chaudhry <aazibch47@gmail.com>
2024-11-01 07:03:14 +0500

    Implement loading UI in singup-form

--- components/auth/SignupForm.tsx
+++ components/auth/SignupForm.tsx
@@ -6,37 +6,40 @@ import { Button, Label, TextInput } from 'flowbite-react';
 import { signIn } from 'next-auth/react';
 
 import styles from './form.module.css';
+import { useFormStatus } from 'react-dom';
 
 export default function SignupForm() {
-  const fullNameRef = useRef<HTMLInputElement>(null);
-  const usernameRef = useRef<HTMLInputElement>(null);
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-  const passwordConfirmationRef = useRef<HTMLInputElement>(null);
+  const { pending } = useFormStatus();
 
-  const formSubmitHandler = async () => {
-    const fullName = fullNameRef.current!.value;
-    const username = usernameRef.current!.value;
-    const email = emailRef.current!.value;
-    const password = passwordRef.current!.value;
-    const passwordConfirmation = passwordConfirmationRef.current!.value;
+  // const fullNameRef = useRef<HTMLInputElement>(null);
+  // const usernameRef = useRef<HTMLInputElement>(null);
+  // const emailRef = useRef<HTMLInputElement>(null);
+  // const passwordRef = useRef<HTMLInputElement>(null);
+  // const passwordConfirmationRef = useRef<HTMLInputElement>(null);
 
-    const res = await signIn('credentials-signup', {
-      fullName,
-      username,
-      email,
-      password,
-      passwordConfirmation
-    });
+  // const formSubmitHandler = async () => {
+  //   const fullName = fullNameRef.current!.value;
+  //   const username = usernameRef.current!.value;
+  //   const email = emailRef.current!.value;
+  //   const password = passwordRef.current!.value;
+  //   const passwordConfirmation = passwordConfirmationRef.current!.value;
 
-    if (res) {
-      console.log('[SignupForm][formSubmitHandler] res', res);
-    }
-  };
+  //   const res = await signIn('credentials-signup', {
+  //     fullName,
+  //     username,
+  //     email,
+  //     password,
+  //     passwordConfirmation
+  //   });
+
+  //   if (res) {
+  //     console.log('[SignupForm][formSubmitHandler] res', res);
+  //   }
+  // };
 
   return (
     <form
-      onSubmit={formSubmitHandler}
+      // onSubmit={formSubmitHandler}
       className="flex max-w-md flex-col gap-4 mx-auto"
     >
       <div>
@@ -44,7 +47,7 @@ export default function SignupForm() {
           <Label htmlFor="fullName" value="Full Name" />
         </div>
         <TextInput
-          ref={fullNameRef}
+          // ref={fullNameRef}
           id="fullName"
           type="text"
           placeholder="John Doe"
@@ -56,7 +59,7 @@ export default function SignupForm() {
           <Label htmlFor="username" value="Username" />
         </div>
         <TextInput
-          ref={usernameRef}
+          // ref={usernameRef}
           id="username"
           type="text"
           placeholder="JohnTheBard"
@@ -68,7 +71,7 @@ export default function SignupForm() {
           <Label htmlFor="email" value="Email" />
         </div>
         <TextInput
-          ref={emailRef}
+          // ref={emailRef}
           id="email"
           type="email"
           placeholder="poet@poetsdomain.com"
@@ -79,14 +82,15 @@ export default function SignupForm() {
         <div className="mb-2 block">
           <Label htmlFor="password" value="Password" />
         </div>
-        <TextInput ref={passwordRef} id="password" type="password" required />
+        {/* <TextInput ref={passwordRef} id="password" type="password" required /> */}
+        <TextInput id="password" type="password" required />
       </div>
       <div>
         <div className="mb-2 block">
           <Label htmlFor="passwordConfirmation" value="Confirm Password" />
         </div>
         <TextInput
-          ref={passwordConfirmationRef}
+          // ref={passwordConfirmationRef}
           id="passwordConfirmation"
           type="password"
           required
@@ -94,7 +98,7 @@ export default function SignupForm() {
       </div>
 
       <Button color="blue" type="submit">
-        Submit
+        {pending ? <span>...Loading</span> : <span>Submit</span>}
       </Button>
       <div className="mt-5">
         <p
