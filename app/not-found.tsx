64a0e660cd89b1ba7b5aff5e5fb1ac8b11ce5276
Aazib Chaudhry <aazibch47@gmail.com>
2024-12-18 09:41:44 +0500

    Create custom not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,21 @@
-'use client';
-
 import Image from 'next/image';
-
-import serverErrorIllustration from '@/assets/images/server-error.svg';
+import notFoundIllustration from '@/assets/images/not-found.svg';
+import Link from 'next/link';
 
 export default function ErrorPage() {
   return (
     <div className="text-center my-20 mx-4">
       <Image
         className="w-[35rem] m-auto mb-20"
-        src={serverErrorIllustration}
+        src={notFoundIllustration}
         alt="Illustration showing a rounded figure holding a disconnected cable what was supposed to be running from the server to the computer"
         priority
       />
-      <p className="text-lg text-gray-600">
-        Something went wrong! Please try again in a while.
+      <p className="text-lg text-gray-600 mb-4">
+        Oh, oh! Looks like the page does not exist.
+      </p>
+      <p className="text-lg">
+        <Link href="/">Return Home</Link>
       </p>
     </div>
   );
