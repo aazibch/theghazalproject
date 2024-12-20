03571caa39f92d463d5689636f87cbb07479214a
Aazib Chaudhry <aazibch47@gmail.com>
2024-12-20 08:22:47 +0500

    Adjust responsive styling for ghazal-couplet component

--- components/home/ghazal-section/ghazal-couplet.tsx
+++ components/home/ghazal-section/ghazal-couplet.tsx
@@ -23,19 +23,18 @@ export default function GhazalCouplet({
       id={id}
       className="flex flex-col-reverse md:flex-row items-center justify-center mb-6 mx-auto text-center"
     >
-      <div className="md:basis-[8rem] md:mr-2">
-        <Avatar rounded img={user.avatar} className="hidden md:flex md:mb-1" />
+      <Link
+        className="text-inherit no-underline hover:no-underline"
+        href={`/users/${user.username}`}
+      >
+        <div className="md:basis-[8rem] md:mr-2 flex flex-row md:flex-col items-center">
+          <Avatar rounded img={user.avatar} className="mr-2 md:mr-0 md:mb-1" />
 
-        <span className="text-sm text-gray-600">
-          <Link
-            className="text-inherit no-underline hover:no-underline"
-            href={`/users/${user.username}`}
-          >
-            {user.fullName}
-          </Link>
-        </span>
-      </div>
-      <div className="font-merriweather font-light md:basis-[26rem] mb-2">
+          <span className="text-sm text-gray-600">{user.fullName}</span>
+        </div>
+      </Link>
+
+      <div className="font-merriweather font-light md:basis-[26rem] mb-4">
         <span className="block mb-1">{couplet[0]}</span>
         <span className="block">{couplet[1]}</span>
       </div>
