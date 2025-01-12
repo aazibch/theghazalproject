795f5af03b6b98abf494419cc710ae93a6f6126a
Aazib Chaudhry <aazibch47@gmail.com>
2025-01-12 18:08:49 +0500

    Revert "Empty commit to test netlify branch deploys"
    This reverts commit 02ee511ce8499df5beae822cf176b1649436ca16.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import './globals.css';
 import Layout from '@/components/layout/layout';
 import AuthProvider from '@/components/auth/auth-provider';
 
-// Testing branch deploys
-
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
