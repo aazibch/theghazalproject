2ce373a2fe180c5599c3be45e7bd83bda4e0b179
Aazib Chaudhry <aazibch47@gmail.com>
2025-04-02 22:25:56 +0500

    Modify hero-section styling

--- components/home/hero-section/hero-section.tsx
+++ components/home/hero-section/hero-section.tsx
@@ -8,12 +8,12 @@ import Banner from '@/components/ui/banner';
 export default function HeroSection() {
   return (
     <section
-      className="p-4 flex justify-center items-center h-[40rem] bg-no-repeat bg-[50%_45%] bg-cover bg-[#edebe6] text-white"
+      className="p-6 flex justify-center items-center h-[40rem] bg-no-repeat bg-[50%_45%] bg-cover bg-[#edebe6] text-white"
       style={{ backgroundImage: 'url(/images/turkish-ceiling-bg.png)' }}
     >
       <div className="text-center max-w-4xl mx-auto">
-        <h1 className="text-4xl mb-10">Learn. Create. Share.</h1>
-        <p className="text-2xl mb-8">
+        <h1 className="text-3xl md:text-4xl mb-10">Learn. Create. Share.</h1>
+        <p className="text-xl md:text-2xl mb-8">
           At <span className="font-semibold">The Ghazal Project</span>, we want
           to build a thriving community of creative individuals from all around
           the world, working together to explore and share the wonders of the
