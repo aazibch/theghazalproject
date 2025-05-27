a43c05cf09f6293665013f9d2236ae1f11ef0c7f
Aazib Chaudhry <aazibch47@gmail.com>
2025-05-27 17:28:28 +0500

    Add 'coming soon' page

--- components/layout/pages/page-container.tsx
+++
@@ -1,7 +0,0 @@
-export default function PageContainer({
-  children
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="max-w-screen-xl mx-auto px-4 py-12">{children}</div>;
-}
--- components/layout/pages/page-header.tsx
+++
@@ -1,8 +0,0 @@
-export default function PageHeader({ heading }: { heading: string }) {
-  return (
-    <header className="py-2 mb-8">
-      <div className="w-7 h-1 mb-2 bg-primary_blue"></div>
-      <h2 className="text-2xl">{heading}</h2>
-    </header>
-  );
-}
