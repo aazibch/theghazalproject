c49f535a8508aead495629b8aac22e2c1cc469d7
Aazib Chaudhry <aazibch47@gmail.com>
2025-06-27 12:10:40 +0500

    Refactor common field schemas into reusable variables

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -2,20 +2,59 @@ import Joi from 'joi';
 
 import { generateValidationMessage } from './generateValidationMessage';
 
+const fullNameInputSchema = Joi.string()
+  .pattern(new RegExp(/^[a-zA-Z ]*$/))
+  .min(3)
+  .max(75)
+  .required()
+  .messages({
+    'string.pattern.base':
+      'The full name may only contain alphabets (letters A-Z) and spaces.',
+    'string.min': generateValidationMessage('min', 'full name', 3),
+    'string.max': generateValidationMessage('max', 'full name', 75),
+    'string.empty': generateValidationMessage('required', 'full name'),
+    'any.required': generateValidationMessage('required', 'full name')
+  });
+
+const emailInputSchema = Joi.string()
+  .min(5)
+  .max(50)
+  .email({ tlds: { allow: false } })
+  .required()
+  .messages({
+    'string.min': generateValidationMessage('min', 'email address', 5),
+    'string.max': generateValidationMessage('max', 'email address', 50),
+    'string.email': generateValidationMessage('email'),
+    'string.empty': generateValidationMessage('required', 'email address'),
+    'any.required': generateValidationMessage('required', 'email address')
+  });
+
+const passwordInputSchema = Joi.string()
+  .min(8)
+  .required()
+  .messages({
+    'string.min': generateValidationMessage('min', 'password', 8),
+    'string.empty': generateValidationMessage('required', 'password'),
+    'any.required': generateValidationMessage('required', 'password')
+  });
+
+const passwordConfirmationInputSchema = Joi.string()
+  .valid(Joi.ref('password'))
+  .required()
+  .messages({
+    'string.empty': generateValidationMessage(
+      'required',
+      'password confirmation'
+    ),
+    'any.required': generateValidationMessage(
+      'required',
+      'password confirmation'
+    ),
+    'any.only': generateValidationMessage('passwordConfirmation')
+  });
+
 export const signupSchema = Joi.object({
-  fullName: Joi.string()
-    .pattern(new RegExp(/^[a-zA-Z ]*$/))
-    .min(3)
-    .max(75)
-    .required()
-    .messages({
-      'string.pattern.base':
-        'The full name may only contain alphabets (letters A-Z) and spaces.',
-      'string.min': generateValidationMessage('min', 'full name', 3),
-      'string.max': generateValidationMessage('max', 'full name', 75),
-      'string.empty': generateValidationMessage('required', 'full name'),
-      'any.required': generateValidationMessage('required', 'full name')
-    }),
+  fullName: fullNameInputSchema,
   username: Joi.string()
     .pattern(new RegExp(/^[a-zA-Z0-9_]*$/))
     .min(3)
@@ -29,96 +68,22 @@ export const signupSchema = Joi.object({
       'string.empty': generateValidationMessage('required', 'username'),
       'any.required': generateValidationMessage('required', 'username')
     }),
-  email: Joi.string()
-    .min(5)
-    .max(50)
-    .email({ tlds: { allow: false } })
-    .required()
-    .messages({
-      'string.min': generateValidationMessage('min', 'email address', 5),
-      'string.max': generateValidationMessage('max', 'email address', 50),
-      'string.email': generateValidationMessage('email'),
-      'string.empty': generateValidationMessage('required', 'email address'),
-      'any.required': generateValidationMessage('required', 'email address')
-    }),
-  password: Joi.string()
-    .min(8)
-    .required()
-    .messages({
-      'string.min': generateValidationMessage('min', 'password', 8),
-      'string.empty': generateValidationMessage('required', 'password'),
-      'any.required': generateValidationMessage('required', 'password')
-    }),
-  passwordConfirmation: Joi.string()
-    .valid(Joi.ref('password'))
-    .required()
-    .messages({
-      'string.empty': generateValidationMessage(
-        'required',
-        'password confirmation'
-      ),
-      'any.required': generateValidationMessage(
-        'required',
-        'password confirmation'
-      ),
-      'any.only': generateValidationMessage('passwordConfirmation')
-    })
+  email: emailInputSchema,
+  password: passwordInputSchema,
+  passwordConfirmation: passwordConfirmationInputSchema
 });
 
 export const updateProfileSettingsSchema = Joi.object({
-  fullName: Joi.string()
-    .pattern(new RegExp(/^[a-zA-Z ]*$/))
-    .min(3)
-    .max(75)
-    .required()
-    .messages({
-      'string.pattern.base':
-        'The full name may only contain alphabets (letters A-Z) and spaces.',
-      'string.min': generateValidationMessage('min', 'full name', 3),
-      'string.max': generateValidationMessage('max', 'full name', 75),
-      'string.empty': generateValidationMessage('required', 'full name'),
-      'any.required': generateValidationMessage('required', 'full name')
-    })
+  fullName: fullNameInputSchema
 });
 
 export const updateAccountEmailSettingsSchema = Joi.object({
-  email: Joi.string()
-    .min(5)
-    .max(50)
-    .email({ tlds: { allow: false } })
-    .required()
-    .messages({
-      'string.min': generateValidationMessage('min', 'email address', 5),
-      'string.max': generateValidationMessage('max', 'email address', 50),
-      'string.email': generateValidationMessage('email'),
-      'string.empty': generateValidationMessage('required', 'email address'),
-      'any.required': generateValidationMessage('required', 'email address')
-    })
+  email: emailInputSchema
 });
 
 export const newPasswordSchema = Joi.object({
-  password: Joi.string()
-    .min(8)
-    .required()
-    .messages({
-      'string.min': generateValidationMessage('min', 'password', 8),
-      'string.empty': generateValidationMessage('required', 'password'),
-      'any.required': generateValidationMessage('required', 'password')
-    }),
-  passwordConfirmation: Joi.string()
-    .valid(Joi.ref('password'))
-    .required()
-    .messages({
-      'string.empty': generateValidationMessage(
-        'required',
-        'password confirmation'
-      ),
-      'any.required': generateValidationMessage(
-        'required',
-        'password confirmation'
-      ),
-      'any.only': generateValidationMessage('passwordConfirmation')
-    })
+  password: passwordInputSchema,
+  passwordConfirmation: passwordConfirmationInputSchema
 });
 
 export const colGhazalEntrySchema = Joi.object({
@@ -158,18 +123,7 @@ export const contactSchema = Joi.object({
       'string.empty': generateValidationMessage('required', 'full name'),
       'any.required': generateValidationMessage('required', 'full name')
     }),
-  email: Joi.string()
-    .min(5)
-    .max(50)
-    .email({ tlds: { allow: false } })
-    .required()
-    .messages({
-      'string.min': generateValidationMessage('min', 'email address', 5),
-      'string.max': generateValidationMessage('max', 'email address', 50),
-      'string.email': generateValidationMessage('email'),
-      'string.empty': generateValidationMessage('required', 'email address'),
-      'any.required': generateValidationMessage('required', 'email address')
-    }),
+  email: emailInputSchema,
   message: Joi.string()
     .min(5)
     .required()
@@ -179,5 +133,3 @@ export const contactSchema = Joi.object({
       'any.required': generateValidationMessage('required', 'message')
     })
 });
-
-// TODO: Refactor common field schemas into reusable variables.
